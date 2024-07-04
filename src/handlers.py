27672fa15e43f8cbe0164c44e7eec6627f196550
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-07-04 16:37:36 +0700

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -12,12 +12,11 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.markdown import *
-from aiogram.types.input_media_photo import *
-from aiogram.types import input_media_photo
 
 from config import *
 from resources.TEXT_MESSAGES import *
 from send_logs import *
+from send_data import *
 from kb_builder import *
 from StatesGroup import *
 
@@ -59,29 +58,43 @@ async def GetUserIdea(callback: types.CallbackQuery, state: FSMContext):
         logger.error(f"{err}")
         await send_log_to_dev()
 
-
 @dp.message(GetIdea.wait_for_message)
 async def IdeaUserMessage(message: Message, state: FSMContext) -> None:
     try:
-        user_username = message.from_user.username
-        forward_text = f"Idea from @{user_username}:\n\n"
-        
-        if message.text:
-            forward_text += message.text
-        
+        user_id = str(message.from_user.id)
+        chat_id = message.chat.id
+        member = await bot.get_chat_member(chat_id, user_id)
+        username = member.user.username
+
         if message.photo:
-            photo = message.photo[-1]
-            caption = message.caption
-            for dev in DEVS:
-                await bot.send_photo(chat_id=dev, photo=photo.file_id, caption=f"{forward_text}\n{caption}" if caption else forward_text)
-        elif message.document:
-            document = message.document
-            caption = message.caption
-            for dev in DEVS:
-                await bot.send_document(chat_id=dev, document=document.file_id, caption=f"{forward_text}\n{caption}" if caption else forward_text)
+            try:
+                file_id = message.photo[-1].file_id
+                file = await bot.get_file(file_id)
+                file_url = file.file_path
+
+                caption = message.caption
+                logger.debug(f"Picture received: {file_url}")
+                logger.info(f"Message: {caption}")
+                
+                await send_messages_with_picture(file_id, caption, state, username, "Idea")
+            except Exception as e:
+                await send_log_to_dev()
+                logger.error(f"Error: {e}")
+
         else:
-            for dev in DEVS:
-                await bot.send_message(chat_id=dev, text=forward_text)
+            logger.debug('No pictures found')
+
+            try:
+                admin_message = str(message.text)
+                logger.info(f"Message: {admin_message}")
+
+                await send_messages(admin_message, state, username, "Idea")
+            except Exception as e:
+                await send_log_to_dev()
+                logger.error(f"Error: {e}")
+
+        await message.answer(DONE_TEXT)
+
 
     except Exception as err:
         logger.error(f"{err}")
@@ -89,27 +102,42 @@ async def IdeaUserMessage(message: Message, state: FSMContext) -> None:
 
 
 @dp.message(GetBug.wait_for_message)
-async def BugUserMessage(message: types.Message, state: FSMContext) -> None:
+async def BugUserMessage(message: Message, state: FSMContext) -> None:
     try:
-        user_username = message.from_user.username
-        forward_text = f"Bug from @{user_username}:\n\n"
-        
-        if message.text:
-            forward_text += message.text
-        
+        user_id = str(message.from_user.id)
+        chat_id = message.chat.id
+        member = await bot.get_chat_member(chat_id, user_id)
+        username = member.user.username
+
         if message.photo:
-            photo = message.photo[-1]
-            caption = message.caption
-            for dev in DEVS:
-                await bot.send_photo(chat_id=dev, photo=photo.file_id, caption=f"{forward_text}\n{caption}" if caption else forward_text)
-        elif message.document:
-            document = message.document
-            caption = message.caption
-            for dev in DEVS:
-                await bot.send_document(chat_id=dev, document=document.file_id, caption=f"{forward_text}\n{caption}" if caption else forward_text)
+            try:
+                file_id = message.photo[-1].file_id
+                file = await bot.get_file(file_id)
+                file_url = file.file_path
+
+                caption = message.caption
+                logger.debug(f"Picture received: {file_url}")
+                logger.info(f"Message: {caption}")
+                
+                await send_messages_with_picture(file_id, caption, state, username, "Bug")
+
+            except Exception as e:
+                await send_log_to_dev()
+                logger.error(f"Error: {e}")
+
         else:
-            for dev in DEVS:
-                await bot.send_message(chat_id=dev, text=forward_text)
+            logger.debug('No pictures found')
+
+            try:
+                admin_message = str(message.text)
+                logger.info(f"Message: {admin_message}")
+
+                await send_messages(admin_message, state, username, "Bug")
+            except Exception as e:
+                await send_log_to_dev()
+                logger.error(f"Error: {e}")
+
+        await message.answer(DONE_TEXT)
 
     except Exception as err:
         logger.error(f"{err}")
