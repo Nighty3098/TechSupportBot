3cd9bb70e353d16f1d7e02d3c95271d59e31ad5f
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-07-04 17:01:19 +0700

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -12,11 +12,12 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.markdown import *
+from aiogram.types.input_media_photo import *
+from aiogram.types import input_media_photo
 
 from config import *
 from resources.TEXT_MESSAGES import *
 from send_logs import *
-from send_data import *
 from kb_builder import *
 from StatesGroup import *
 
@@ -58,86 +59,32 @@ async def GetUserIdea(callback: types.CallbackQuery, state: FSMContext):
         logger.error(f"{err}")
         await send_log_to_dev()
 
+
 @dp.message(GetIdea.wait_for_message)
 async def IdeaUserMessage(message: Message, state: FSMContext) -> None:
     try:
-        user_id = str(message.from_user.id)
-        chat_id = message.chat.id
-        member = await bot.get_chat_member(chat_id, user_id)
-        username = member.user.username
+        username = message.from_user.username
 
-        if message.photo:
+        for dev_id in DEVS:
             try:
-                file_id = message.photo[-1].file_id
-                file = await bot.get_file(file_id)
-                file_url = file.file_path
-
-                caption = message.caption
-                logger.debug(f"Picture received: {file_url}")
-                logger.info(f"Message: {caption}")
-                
-                await send_messages_with_picture(file_id, caption, state, username, "Idea")
+                await message.forward(dev_id)
             except Exception as e:
-                await send_log_to_dev()
-                logger.error(f"Error: {e}")
-
-        else:
-            logger.debug('No pictures found')
-
-            try:
-                admin_message = str(message.text)
-                logger.info(f"Message: {admin_message}")
-
-                await send_messages(admin_message, state, username, "Idea")
-            except Exception as e:
-                await send_log_to_dev()
-                logger.error(f"Error: {e}")
-
-        await message.answer(DONE_TEXT)
-
-
+                logging.error(f"Error forwarding message to developer {dev_id}: {e}")
     except Exception as err:
         logger.error(f"{err}")
         await send_log_to_dev()
 
 
 @dp.message(GetBug.wait_for_message)
-async def BugUserMessage(message: Message, state: FSMContext) -> None:
+async def BugUserMessage(message: types.Message, state: FSMContext) -> None:
     try:
-        user_id = str(message.from_user.id)
-        chat_id = message.chat.id
-        member = await bot.get_chat_member(chat_id, user_id)
-        username = member.user.username
+        username = message.from_user.username
 
-        if message.photo:
+        for dev_id in DEVS:
             try:
-                file_id = message.photo[-1].file_id
-                file = await bot.get_file(file_id)
-                file_url = file.file_path
-
-                caption = message.caption
-                logger.debug(f"Picture received: {file_url}")
-                logger.info(f"Message: {caption}")
-                
-                await send_messages_with_picture(file_id, caption, state, username, "Bug")
-
+                await message.forward(dev_id)
             except Exception as e:
-                await send_log_to_dev()
-                logger.error(f"Error: {e}")
-
-        else:
-            logger.debug('No pictures found')
-
-            try:
-                admin_message = str(message.text)
-                logger.info(f"Message: {admin_message}")
-
-                await send_messages(admin_message, state, username, "Bug")
-            except Exception as e:
-                await send_log_to_dev()
-                logger.error(f"Error: {e}")
-
-        await message.answer(DONE_TEXT)
+                logging.error(f"Error forwarding message to developer {dev_id}: {e}")
 
     except Exception as err:
         logger.error(f"{err}")
